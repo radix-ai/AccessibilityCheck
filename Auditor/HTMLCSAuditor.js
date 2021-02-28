0e3371792b30331ad617534087b78a0a94d93827
Giel Dops <gieldops@hotmail.com>
2021-02-28 22:33:02 +0100

    Use the more used 127.0.0.1

--- Auditor/HTMLCSAuditor.js
+++ Auditor/HTMLCSAuditor.js
@@ -1857,7 +1857,7 @@ _global.HTMLCSAuditor = new function()
             );  
             console.log(json_object);
             $.ajax({
-                url: "http://0.0.0.0:8000/v1/check_url/",
+                url: "http://127.0.0.1:8000/v1/check_url/",
                 // url: "https://accessibility-check.radix.sh/v1/check_url/",
                 type: 'POST',
                 contentType: 'application/json',
